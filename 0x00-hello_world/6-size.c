43576aeef611d5087205acc5f2169516b67b7506
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-07 10:47:43 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long int);
+	sizeof(long long int);
+	sizeof(float);
+	printf("size of a %d\n", char, int, long int, long long int, float);
+	return (0);
+}
