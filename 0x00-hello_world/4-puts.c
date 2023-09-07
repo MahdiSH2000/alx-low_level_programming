97fb2a78370a42eca3e9a6b3772b4322b0ef39fb
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-07 09:40:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-	/*The main function is the entry point of the program.*/
+/**
+ * The main function is the entry point of the program.
+ *
+ * @return 0 on successful execution.
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
