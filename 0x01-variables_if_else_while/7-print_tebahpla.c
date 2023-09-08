477fb38d88ad3e29b1bb5f70c33ebce6800728e1
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-08 11:02:58 -0700

    enverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
