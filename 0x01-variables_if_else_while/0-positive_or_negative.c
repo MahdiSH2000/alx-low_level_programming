053f5961d1a28b8ee1ee91b971d8850fd047b341
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-08 07:54:05 -0700

    + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - entry point */
+/**
+ *  main - entry point
+ *  description: A C program that print with put function
+ */
 
 /* Return: always 0 */
 int main(void)
