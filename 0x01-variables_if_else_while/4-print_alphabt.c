3ac48fff9935a75f98cfe96bbe221f7a6dc0626c
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-08 10:32:25 -0700

    printwithouteq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i == 'q' || i == 'e')
+			i++;
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
