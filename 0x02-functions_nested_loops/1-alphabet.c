6f28979f78c563f3c641690c2ad486eaa765057e
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-12 06:42:02 -0700

    1alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
