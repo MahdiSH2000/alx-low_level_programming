50390e5b67801e98c594150fa776e3245a1f8c79
MahdiSH2000 <mahdishaimi2000@gmail.com>
2023-09-12 06:29:58 -0700

    0putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
